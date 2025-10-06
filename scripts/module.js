4c371ac1670994af19141ed5cb1169e85c6c78c6
cadowtin <chasdowtin@gmail.com>
2025-10-06 15:44:03 -0500

    0.7.0
    - **Added**
      - `Character Campaign Notes`
        - Adds a new hotkey (Default `N`) to open an editable dialog of their assigned character's `Campaign Notes` field on their character sheet for quick access

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,6 @@
 import { getSetting } from "./lib/helpers.js";
 import { minifySimpleRequests } from "./lib/simpleRequests.js";
-import { loadAllTemplates, setupSettings } from "./settings.js";
+import { loadAllTemplates, registerKeybindings, setupSettings } from "./settings.js";
 import { hideDefaultCraftChecks, hideSellAllTreasure } from "./lib/sheetTweaks.js";
 import { setupColorizeToolbeltMessageSaves, setupHighlightToolbeltRollSaves } from "./lib/pf2eToolbelt.js";
 import { setupColorizePersistentPF2eHUD } from "./lib/pf2eHUD.js";
@@ -14,6 +14,7 @@ export const MODULE_ID = 'sundry';
 
 Hooks.once('init', async function () {
     setupSettings();
+    registerKeybindings();
     loadAllTemplates();
 });
 
